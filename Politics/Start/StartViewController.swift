bd0457a41d1d006a50c4ceb1d710bccf9ce08985
tatsumi kentaro <tatsuken4290@gmail.com>
2018-11-08 09:26:34 +0900

    登録画面変更、処理変更

--- Politics/Start/StartViewController.swift
+++ Politics/Start/StartViewController.swift
@@ -10,145 +10,133 @@ import UIKit
 import RealmSwift
 import Firebase
 import RealmSwift
-class StartViewController: UIViewController,UIPickerViewDataSource,UIPickerViewDelegate,UITextFieldDelegate {
-    @IBOutlet weak var userName: UITextField!
-    @IBOutlet weak var placeTextField: UITextField!
-    @IBOutlet weak var ageTextField: UITextField!
+import Eureka
+import ViewRow
+class StartViewController: FormViewController  {
+
     var sex:String!
     let saveData:UserDefaults = UserDefaults.standard
-    let placeArray = [
-        "北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
-        "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
-        "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
-        "静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
-        "奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
-        "徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
-        "熊本県","大分県","宮崎県","鹿児島県","沖縄県"
-    ]
-    let ageArray = [
-        "~20歳","20歳~24歳","25歳~29歳","30歳~34歳","35歳~39歳","40歳~44歳","45歳~49歳","50歳~54歳","55歳~59歳","60歳~64歳","65歳~69歳","70歳~74歳","75歳~79歳","80歳以上"
-    ]
+
     var db : Firestore!
     let realm = try! Realm()
     override func viewDidLoad() {
         super.viewDidLoad()
         db = Firestore.firestore()
-        let pickerView1 = UIPickerView()
-        let pickerView2 = UIPickerView()
-        pickerView1.tag = 1
-        pickerView2.tag = 2
-        pickerView1 .dataSource = self
-        pickerView2.dataSource = self
-        pickerView1.delegate = self
-        pickerView2.delegate = self
-        userName.delegate = self
-        placeTextField.inputView = pickerView1
-        ageTextField.inputView = pickerView2
-        let toolbar = UIToolbar(frame: CGRect(x: 0, y: 0, width: 0, height: 40))
-        let doneItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(self.done))
-        toolbar.setItems([doneItem], animated: true)
-        placeTextField.inputAccessoryView = toolbar
-        ageTextField.inputAccessoryView = toolbar
-        
+        form
+            +++ Section()
+            <<< ViewRow<UIImageView>("view") { (row) in
+                
+                }
+                .cellSetup { (cell, row) in
+                    //  Construct the view for the cell
+                    cell.textLabel?.textAlignment = .center
+                    cell.view = UIImageView()
+                    cell.view?.contentMode = .scaleAspectFit
+                    cell.contentView.addSubview(cell.view!)
+                    cell.backgroundColor = UIColor(rgb: (r: 243, g: 243, b: 242))
+                    
+                    //  Get something to display
+                    let image = UIImage(named: "voting")
+                    
+                    cell.view!.image = image
+                    
+                    //  Make the image view occupy the entire row:
+                    cell.viewRightMargin = 0.0
+                    cell.viewLeftMargin = 0.0
+                    cell.viewTopMargin = 0.0
+                    cell.viewBottomMargin = 0.0
+                    //  Define the cell's height
+                    cell.height = { return CGFloat(200) }
+            }
+
+            +++ Section("プロフィール入力")
+            <<< TextRow("name"){
+                $0.placeholder = "名前を入力"
+        }
+            <<< SegmentedRow<String>("sex") { $0.options = ["男性", "女性", "その他"] }
+            <<< PushRow<String>("place") {
+                $0.title = "都道府県"
+                $0.options = ["","北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県",
+                              "茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県",
+                              "新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
+                              "静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県",
+                              "奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県",
+                              "徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県",
+                              "熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+                $0.value = ""
+                $0.selectorTitle = "Choose an Emoji!"
+        }
+            <<< PushRow<String>("age") {
+                $0.title = "年齢"
+                $0.options = ["","~20歳","20歳~24歳","25歳~29歳","30歳~34歳","35歳~39歳","40歳~44歳","45歳~49歳","50歳~54歳","55歳~59歳","60歳~64歳","65歳~69歳","70歳~74歳","75歳~79歳","80歳以上"]
+                $0.value = ""
+                $0.selectorTitle = "Choose an Emoji!"
+                }
+            <<< ButtonRow(){
+                $0.title = "登録する"
+                $0.cell.tintColor = UIColor.orange
+                }.onCellSelection({ i, to in
+                    print(self.form.values())
+                    let array = self.form.values()
+                    guard let name:String = array["name"] as? String,let place:String = array["place"] as? String,let age:String = array["age"] as? String else{
+                        self.alert(message: "名前を入力してください")
+                        return
+                    }
+                    guard let sex:String = array["sex"] as? String,sex != "" else{
+                        self.alert(message: "性別を入力してください")
+                        return
+                    }
+                    
+                    guard place != "" else{
+                        self.alert(message: "場所を入力してください")
+                        return
+                    }
+                    guard age != "" else{
+                        self.alert(message: "年齢を入力してください")
+                        return
+                    }
+                    print(name)
+                    print(sex)
+                    print(place)
+                    print(age)
+   
+                    var ref: DocumentReference? = nil
+                    ref = self.db.collection("users").addDocument(data: [
+                        "name":name,
+                        "place":place,
+                        "age":age,
+                        "sex":sex
+                    ]){ err in
+                        if let err = err {
+                            print("Error adding document: \(err)")
+                        } else {
+                            print("Document added with ID: \(ref!.documentID)")
+                            let user = Userdata()
+                            user.age = age
+                            user.name = name
+                            user.place = place
+                            user.sex = sex
+                            user.userID = "\(ref!.documentID)"
+                            try! self.realm.write() {
+                                self.realm.add(user)
+                            }
+                            self.saveData.set(true, forKey: "user")
+                        }
+                    }
+
+                    let tab = self.tabSegue()
+                    self.present(tab, animated: true, completion: nil)
+
+                    
+                })
         // Do any additional setup after loading the view.
     }
     @objc func done() {
         view.endEditing(true)
     }
-    
-    
-    
-    func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        switch pickerView.tag {
-        case 1:
-            return placeArray.count
-        case 2:
-            return ageArray.count
-        default:
-            return 0
-        }
-    }
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        switch pickerView.tag {
-        case 1:
-            return placeArray[row]
-        case 2:
-            return ageArray[row]
-        default:
-            return "nil"
-        }
-    }
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        switch pickerView.tag {
-        case 1:
-            placeTextField.text = placeArray[row]
-        case 2:
-            ageTextField.text = ageArray[row]
-        default:
-            return
-        }
-    }
-    @IBAction func selectSegment(_ sender: UISegmentedControl) {
-        switch sender.selectedSegmentIndex {
-        case 0:
-            sex = "boy"
-        case 1:
-            sex = "girl"
-        case 3:
-            sex = "other"
-        default:
-            return
-        }
-    }
-    
-    @IBAction func decide(_ sender: Any) {
-        if let name = userName.text,let place = placeTextField.text,let age = ageTextField.text,sex != nil{
-                        var ref: DocumentReference? = nil
-                        ref = db.collection("users").addDocument(data: [
-                             "name":name,
-                             "place":place,
-                             "age":age,
-                             "sex":sex
-                        ]){ err in
-                            if let err = err {
-                                print("Error adding document: \(err)")
-                            } else {
-                                print("Document added with ID: \(ref!.documentID)")
-                                let user = Userdata()
-                                user.age = age
-                                user.name = name
-                                user.place = place
-                                user.sex = self.sex
-                                user.userID = "\(ref!.documentID)"
-                                try! self.realm.write() {
-                                    self.realm.add(user)
-                                }
-                                self.saveData.set(true, forKey: "user")
-                            }
-                        }
-            
-            let tab = tabSegue()
-            self.present(tab, animated: true, completion: nil)
-            
-        }else{
-            alert(message: "何かが記入されていません")
-        }
-    }
-    
-    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-        userName.resignFirstResponder()
-        return true
-    }
-    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    
 }
 
